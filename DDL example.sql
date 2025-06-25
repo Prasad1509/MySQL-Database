0db865c7e50444b1c9d67cb6d9ddfa7e22ea92b9
Prasad Shinde <prasadshinde150904@gmail.com>
2025-06-25 21:39:31 +0530

    Add files via upload

---
+++ DDL example.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS school_db;
+
+USE school_db;
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT DEFAULT 18 CHECK (age > 0),
+    class VARCHAR(20) DEFAULT 'FYBSc'
+);
+
+ALTER TABLE students ADD email VARCHAR(100);
+
+ALTER TABLE students ALTER age SET DEFAULT 18;
+
+ALTER TABLE students DROP COLUMN email;
+
+INSERT INTO students (student_id, name, age, class) VALUES
+(1, 'Atharv', 19, 'FYBSc'),
+(2, 'Prasad', 20, 'SYBSc'),
+(3, 'Riya', 18, 'FYBSc'),
+(4, 'Karan', 21, 'TYBSc');
+
+INSERT INTO students (student_id, name, class)
+VALUES (5, 'Sneha', 'SYBSc');
+
+SELECT * FROM students;
+
+
