c735cd87608ad167bcede6c56fda2eb1a822c9f8
Prasad Shinde <prasadshinde150904@gmail.com>
2025-06-28 21:49:52 +0530

    Update Aggrigate function.sql

--- Aggrigate function.sql
+++ Aggrigate function.sql
@@ -16,7 +16,29 @@ SELECT MAX(age) AS oldest FROM students;
 SELECT MIN(age) AS youngest FROM students;
 SELECT class, COUNT(*) AS student_count
 FROM students
+GROUP BY class;SELECT class, AVG(age) AS average_ageUSE school_db;
+
+INSERT INTO students (student_id, name, age, class)
+VALUES 
+(6, 'Omkar', 19, 'FYBSc'),
+(7, 'Neha', 20, 'SYBSc'),
+(8, 'Sameer', 21, 'TYBSc'),
+(9, 'Payal', 20, 'SYBSc'),
+(10, 'Snehal', 19, 'FYBSc');
+
+
+SELECT COUNT(*) AS total_students FROM students;
+SELECT SUM(age) AS total_age FROM students;
+SELECT AVG(age) AS average_age FROM students;
+SELECT MAX(age) AS oldest FROM students;
+SELECT MIN(age) AS youngest FROM students;
+SELECT class, COUNT(*) AS student_count
+FROM students
 GROUP BY class;SELECT class, AVG(age) AS average_age
+FROM students
+GROUP BY class;
+
+
 FROM students
 GROUP BY class;
 
