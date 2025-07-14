7d038a2922402cf5354c9443a501346622796439
Prasad Shinde <prasadshinde150904@gmail.com>
2025-07-14 15:21:23 +0530

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -11,4 +11,5 @@ VALUES
 (1, 'Atharv', 19, 'Full Stack Development'),
 (2, 'Prasad', 20, 'Data Science'),
 (3, 'Riya', 18, 'Web Development');
-SELECT * FROM students;
+SELECT * FROM students;CREATE DATABASE student_db;
+USE student_db;
