61a1a8900c55d04be90a854f8b52ed1f03f2b048
Prasad Shinde <prasadshinde150904@gmail.com>
2025-07-21 10:18:47 +0530

    Update Functio4.sql

--- Functio4.sql
+++ Functio4.sql
@@ -5,7 +5,20 @@ delimiter //
 create function get_full_name(fname varchar(50), lname varchar(50))
 returns varchar(100)
 deterministic
+begindrop function if exists get_full_name;
+
+delimiter //
+
+create function get_full_name(fname varchar(50), lname varchar(50))
+returns varchar(100)
+deterministic
 begin
+  return concat(fname, ' ', lname);
+end //
+
+delimiter ;
+select get_full_name('atharv', 'shinde') as full_name;
+
   return concat(fname, ' ', lname);
 end //drop function if exists get_full_name;
 
