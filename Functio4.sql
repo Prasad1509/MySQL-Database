553028a10ef243c800802fbfda1f379cf596376c
Prasad Shinde <prasadshinde150904@gmail.com>
2025-07-21 10:18:59 +0530

    Update Functio4.sql

--- Functio4.sql
+++ Functio4.sql
@@ -6,7 +6,7 @@ create function get_full_name(fname varchar(50), lname varchar(50))
 returns varchar(100)
 deterministic
 begindrop function if exists get_full_name;
-
+ 
 delimiter //
 
 create function get_full_name(fname varchar(50), lname varchar(50))
